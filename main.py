d6ce200dfb13f0635e61a18e9c3cf034ddce4e06
Sachin Ramanathan <sachin.ramanathan123@gmail.com>
2020-07-03 16:59:22 +0800

    Added few statistics and test
    Added total messages, first and last message dates, and total days of chat. Also added tests for the same

--- main.py
+++ main.py
@@ -2,27 +2,76 @@ import pandas as pd
 from datetime import datetime
 
 
-def parse_chat(content: str) -> pd.DataFrame:
-    """ Parses content and returns a Dataframe with contact, datetime object and message """
-    lines = content.splitlines()
-    if "end-to-end encryption" in lines[0]:
-        lines.pop(0)  # Remove message about end-to-end encryption
-    messages = []
-    for line in lines:
-        try:
-            date_str = line.split(' - ')[0]
-            contact = line.split(' - ')[1].split(':')[0]
-            message = line.split(':')[2].lstrip(' ')
-            date = datetime.strptime(date_str, "%d/%m/%Y, %I:%S %p")
-        except:  # For multi-line messages
-            prev_msg = messages[-1].get('message') + "\n"
-            messages[-1]['message'] = prev_msg + message
+class WhatsAppAnalyzer():
+    """ Analyze WhatsApp data and get statistics """
+    def __init__(self, content=None, df=None):
+        """
+        Either content or dataframe is required
+        """
+        if df:
+            self.df = df
+        elif not content and not df:
+            raise AssertionError("No Dataframe or content passed")
         else:
-            message_data = {
-                'contact': contact,
-                'date': date,
-                'message': message
-            }
-            messages.append(message_data)
-    df = pd.DataFrame(messages)
-    return df
+            self.df = self.parse_chat(content)
+
+    def parse_chat(self, content) -> pd.DataFrame:
+        """ Parses content and returns a Dataframe with contact, datetime object and message """
+        lines = content.splitlines()
+        if "end-to-end encryption" in lines[0]:
+            lines.pop(0)  # Remove message about end-to-end encryption
+        messages = []
+        for line in lines:
+            try:
+                date_str = line.split(' - ')[0]
+                contact = line.split(' - ')[1].split(':')[0]
+                message = line.split(':')[2].lstrip(' ')
+                date = datetime.strptime(date_str, "%d/%m/%Y, %I:%S %p")
+            except:  # For multi-line messages
+                prev_msg = messages[-1].get('message') + "\n"
+                messages[-1]['message'] = prev_msg + message
+            else:
+                message_data = {
+                    'contact': contact,
+                    'date': date,
+                    'message': message
+                }
+                messages.append(message_data)
+        df = pd.DataFrame(messages)
+        return df
+
+    @property
+    def first_message_date(self) -> pd.Timestamp:
+        """
+        Get day of first message as a pandas Timestamp object
+        """
+        first = self.df.at[0, 'date']
+        return first
+
+    @property
+    def last_message_date(self) -> pd.Timestamp:
+        """
+        Get day of last message as a pandas Timestamp object
+        """
+        last = self.df.at[len(self.df) - 1, 'date']
+        return last
+
+    @property
+    def total_days(self) -> int:
+        """
+        Get total number of days between first and last message
+        """
+        days = (self.last_message_date - self.first_message_date).days
+        return days
+
+    @property
+    def total_messages(self) -> int:
+        """ Get total number of messages """
+        return len(self.df)
+
+
+if __name__ == "__main__":
+    file = 'sample_chat/sample_chat.txt'  # Chat data exported from WhatsApp
+    with open(file) as f:
+        content = f.read()
+    analyzer = WhatsAppAnalyzer(content)
--- tests/main_tests.py
+++ tests/main_tests.py
@@ -1,6 +1,8 @@
 import unittest
 import sys
 import os
+from datetime import datetime
+from testconfig import TOTAL_DAYS, TOTAL_MESSAGES, FIRST_MESSAGE_DATE, LAST_MESSAGE_DATE
 
 # Append package directory to path to import modules
 PACKAGE_DIR = os.path.abspath(os.path.join(__file__, '../..'))
@@ -8,14 +10,21 @@ sys.path.append(PACKAGE_DIR)
 
 
 class TestMain(unittest.TestCase):
-    def test_parse_file(self):
+    def test_statistics(self):
         """ Test the parsing of the sample WhatsApp chat """
-        from main import parse_chat
+        from main import WhatsAppAnalyzer
         file = 'sample_chat/sample_chat.txt'  # Chat data exported from WhatsApp
         with open(file) as f:
             content = f.read()
-        df = parse_chat(content)
-        self.assertEqual(len(df), 128)  # Change this if sample chat is changed
+        self.wa = WhatsAppAnalyzer(content)
+        self.assertEqual(self.wa.total_messages, TOTAL_MESSAGES)
+        self.assertEqual(
+            self.wa.first_message_date,
+            datetime.strptime(FIRST_MESSAGE_DATE, "%d/%m/%Y, %I:%S %p"))
+        self.assertEqual(
+            self.wa.last_message_date,
+            datetime.strptime(LAST_MESSAGE_DATE, "%d/%m/%Y, %I:%S %p"))
+        self.assertEqual(self.wa.total_days, TOTAL_DAYS)
 
 
 if __name__ == "__main__":
